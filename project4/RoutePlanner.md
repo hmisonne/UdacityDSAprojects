2264806d8c9e21511a7f362b0a7f4a6f7e24b397
hmisonne <helene.misonne@gmail.com>
2020-02-20 15:36:54 -0800

    Added Priority Queue

--- project4/RoutePlanner.md
+++ project4/RoutePlanner.md
@@ -1,10 +1,11 @@
 # Implementing a Route Planner
 
-To implement a route planner I used a dictionary to have an instant lookup of visited intersections and I used a list of tuples to store all the potential paths. 
-The first element of the tuple is the a combination of estimated and actual distance from start to goal, which allows me to get the shortest distance easily by sorting the tuples.
+To implement a route planner I used a dictionary to have an instant lookup of visited intersections and I used a min priority list (implemented with a heap) to store all the potential paths. The potential paths are tuples with 3 elements: total distance, distance from start, list of intersections.
+The first element of the tuple is the a combination of estimated and actual distance from start to goal, which allows me to get the shortest distance easily by popping the first element of the Heap (or Min Priority Queue).
+Having a priority queue allows to reduce time complexity of getting the smallest element from O(n log(n)) of sorting an array to O(log(n))
 
 n = total number of edges
-The time complexity of this algorithm is O(n) worst case if all the intersections are located in between start and finish.
+The time complexity of evaluating the shortest path is O(n) worst case if all the intersections are located in between start and finish. 
 m = number of potential paths between start and finish
 The space complexity of this algorithm is O(m)
 
--- project4/student_code.py
+++ project4/student_code.py
@@ -1,5 +1,5 @@
 from test import test
-
+import heapq 
 import math
 
 def calculate_distance(M,a,b):
@@ -20,36 +20,33 @@ def shortest_path(M,start,goal):
     path = [(f, g, [start])]
     current_intersection = start
     explored_intersections = {}
-    
+    heapq.heapify(path) 
     if start == goal:
         return [goal]
     
-    while current_intersection != goal:
-        #  Mark current intersection as visited
-        explored_intersections[current_intersection] = True
+    while True:
+        current_path = heapq.heappop(path)
+        current_intersection = current_path[2][-1]
+        if current_intersection != goal:
+            #  Mark current intersection as visited
+            explored_intersections[current_intersection] = True
+
+            # Get the shortest path and distance from start     
+            current_distance = current_path[1]
+
+            # Loop through intersections of current_instersection
+            for intersection in M.roads[current_intersection]:
+                if intersection not in explored_intersections:
+                    # Add a new path
+                    g = current_distance + calculate_distance(M, current_intersection, intersection)
+                    h = calculate_distance(M, intersection, goal)
+                    f = g + h
+                    new_path = current_path[2]+[intersection]
+                    heapq.heappush(path,(f, g, new_path))
+
+        else:
+            return current_path[2]
+                 
         
-        # Get the shortest path and distance from start     
-        current_path = path.pop(0)
-        current_distance = current_path[1]
-
-        # Loop through intersections of current_instersection
-        for intersection in M.roads[current_intersection]:
-            if intersection not in explored_intersections:
-                # Add a new path
-                g = current_distance + calculate_distance(M, current_intersection, intersection)
-                h = calculate_distance(M, intersection, goal)
-                f = g + h
-                new_path = current_path[2]+[intersection]
-                path.append((f, g, new_path)) 
-        # Sort the list to get the shortest path on the first position
-        path = sorted(path)
-        # Update the current_intersection to match the current shortest path
-        current_intersection = path[0][2][-1]     
-        
-    shorted_path = sorted(path)[0][2]
-    return shorted_path
-
-
-
 
 test(shortest_path)
